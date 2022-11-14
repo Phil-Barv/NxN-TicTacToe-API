a48e43eb9006a794ea594156931a01bbf48faaf6
Phil-Barv <56893671+Phil-Barv@users.noreply.github.com>
2022-11-14 00:01:15 -0300

    initial commit

---
+++ api.py
@@ -0,0 +1,38 @@
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+from game import TicTacToe
+
+@app.route('/', methods=["GET"])
+def index():
+    return jsonify({"state": a.board})
+
+@app.route('/get-board', methods=["POST"])
+def get_board():
+    data = request.get_json()
+    data = int(data['size'])
+    global a
+    a = TicTacToe(data)
+
+    return jsonify({"state": a.board, "win":a.check_win(a.board)})
+
+@app.route('/human-play', methods=["POST"])
+def human_play():
+    data = request.get_json()
+    res = a.play_human(data["pos"], data["value"])
+    print("\n\n\n", res)
+ 
+    return jsonify({"play_state": res, "win":a.check_win(res)})
+
+@app.route('/ai-play', methods=["POST"])
+def ai_play():
+    data = request.get_json()
+    res = a.play_ai(data["state"], data["value"])
+    
+    return jsonify({"play_state": res, "win": a.check_win(res)})
+
+app.run(debug=True)
---
+++ game.py
@@ -0,0 +1,100 @@
+# class Cell:
+#     def __init__(self, val, i, j):
+#         #either an X, O, or -
+#         self.value = val
+
+#         #the (i, j) position in the board
+#         self.position = (i, j)
+
+#     # def __repr__(self):
+#     #     return f"Cell({self.position}, {self.value})"
+
+#     def same(self, other):
+#         return self.value == other.value
+from random import choice
+from math import sqrt
+
+class TicTacToe:
+    def __init__(self, size):
+        self.board = {}
+
+        for i in range(size):
+            for j in range(size):
+                self.board[f"({i},{j})"] = "—"
+
+    def board(self):
+        return self.board
+
+    def play_human(self, pos, move):
+        key = f"({pos[0]},{pos[1]})"
+
+        if key in self.board.keys():
+            self.board[key] = move
+        
+        # self.board["(1,1)"] = "O"
+        return self.board
+
+    def play_ai(self, state, val):
+
+        temp = []
+
+        for key, value in state.items():
+            if value == "—":
+                temp.append(key)
+
+        if len(temp) > 0:
+            play = choice(temp)
+            self.board[play] = val
+
+        return self.board
+
+    def check_win(self, state):
+        lst = list(state.keys())
+        n = int(sqrt(len(lst)))
+
+        #check rows
+        for i in range(n):
+            for j in range(n-1):
+                if state[f"({i},{j})"] == "—" or state[f"({i},{j+1})"] == "—" or state[f"({i},{j})"] != state[f"({i},{j+1})"]:
+                    break
+            else:
+                return True
+
+
+        #check columns
+        for j in range(n):
+            for i in range(n-1):
+                if state[f"({i},{j})"] == "—" or state[f"({i+1},{j})"] == "—" or state[f"({i},{j})"] != state[f"({i+1},{j})"]:
+                    break
+            else:
+                return True
+
+
+        #check left diagonal
+        for i in range(n-1):
+            if state[f"({i},{i})"] == "—" or state[f"({i+1},{i+1})"] == "—" or state[f"({i},{i})"] != state[f"({i+1},{i+1})"]:
+                break
+        else:
+            return True
+
+        #check right diagonal
+        for i in range(n -1):
+            empty = state[f"({i},{n-i-1})"] == "—" or state[f"({i+1},{n-i-2})"] == "—"
+            diff = state[f"({i},{n-i-1})"] != state[f"({i+1},{n-i-2})"]
+
+            if empty or diff:
+                break
+
+        else:
+            return True
+
+
+        #draw 
+        if "—" not in list(state.values()):
+            return "Draw"
+
+        #no win 
+        return False
+
+
+
